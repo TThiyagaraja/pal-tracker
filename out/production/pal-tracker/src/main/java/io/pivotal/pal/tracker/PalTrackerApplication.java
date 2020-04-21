9c101b09c5a6addc93f00ba0f3e541d5cb5fa03c
TThiyagaraja <Thiyagaraja@cognizant.com>
2020-04-21 09:45:55 +0530

    All Files associated to First Task

---
+++ out/production/pal-tracker/src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -0,0 +1,8 @@
+package io.pivotal.pal.tracker;
+
+@SpringBootApplication
+public class PalTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PalTrackerApplication.class, args);
+    }
+}
